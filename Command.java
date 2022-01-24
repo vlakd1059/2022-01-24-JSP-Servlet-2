cdecd98bfd8d505b1189f6527dd886d4d76ecffa
박건하 <49181149+vlakd1059@users.noreply.github.com>
2022-01-24 15:08:44 +0900

    // 디자인패턴 예제

---
+++ Command.java
@@ -0,0 +1,12 @@
+package com.pattern;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Command {
+	public void execute(HttpServletRequest request, HttpServletResponse reponse)throws ServletException, IOException;
+	
+}
